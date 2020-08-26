1a9213a7cd99aef693997d9c0e8847059796118e
Yan Jiang <yanjiang@Yans-MacBook-Air.local>
2020-08-26 14:55:49 +0200

    bug exists with firebase

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,27 +1,48 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Image,
-} from "react-native";
-
+import { Platform, KeyboardAvoidingView, SafeAreaView } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
+import Fire from "../Fire";
 export default class ChatScreen extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hi</Text>
-      </View>
+  state = {
+    messages: [],
+  };
+  get user() {
+    return {
+      _id: Fire.uid,
+      name: this.props.navigation.state.params.name,
+    };
+  }
+  componentDidMount() {
+    Fire.get((message) =>
+      this.setState((previous) => ({
+        messages: GiftedChat.append(previous.message, message),
+      }))
     );
   }
+  componentWillUnmount() {
+    Fire.off();
+  }
+  render() {
+    console.log("user", this.user);
+    const chat = (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={Fire.send}
+        user={this.user}
+      />
+    );
+    if (Platform.OS === "android") {
+      return (
+        <KeyboardAvoidingView
+          style={{ flex: 1 }}
+          behavior="padding"
+          keyboardVerticalOffset={30}
+          enabled
+        >
+          {chat}
+        </KeyboardAvoidingView>
+      );
+    }
+    return <SafeAreaView style={{ flex: 1 }}>{chat}</SafeAreaView>;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F4F5F7",
-    justifyContent: "center",
-  },
-});
